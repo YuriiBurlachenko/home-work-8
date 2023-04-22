c8dbf1e0466fa6cb90e268b3b34e81ee09660b3d
YuriiBurlachenko <burlachenkoyurii@gmail.com>
2023-04-22 17:30:38 +0300

    changes variables names

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const DELETEFrom2To4Elements = 1;
-const threeElemetns = 3;
-const arrNum = prompt('введіть новий масив через пробіл');
-console.log(arrNum);
-const arr = arrNum.split(' ');
+const delete_from_2_to_4_elements = 1;
+const three_elemetns = 3;
+const arr_num = prompt('введіть новий масив через пробіл');
+console.log(arr_num);
+const arr = arr_num.split(' ');
 arr.length = prompt('введіть довжину масив');
 console.log(arr);
-let arrSort = arr.sort(function (a, b) {
+let arr_sort = arr.sort(function (a, b) {
   return a - b;
 });
-console.log(arrSort);
-const removed = arrSort.splice(DELETEFrom2To4Elements, threeElemetns);
+console.log(arr_sort);
+const removed = arr_sort.splice(delete_from_2_to_4_elements, three_elemetns);
 console.log('видалені елементи');
 console.log(removed);
 console.log('результат після видаленя');
-console.log(arrSort);
+console.log(arr_sort);
