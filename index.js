ab3c75c7d630621e03649723415f0bbfc2482b91
YuriiBurlachenko <burlachenkoyurii@gmail.com>
2023-04-23 00:10:08 +0300

    change names of variables

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const DELETE_FROM_2_TO_4_ELEMENTS = 1;
 const THREE_ELEMENTS = 3;
+
 const arrNum = prompt('введіть новий масив через пробіл');
+
 console.log(arrNum);
+
 const arr = arrNum.split(' ');
+
 arr.length = prompt('введіть довжину масив');
 console.log(arr);
+
 let arrSort = arr.sort(function (a, b) {
   return a - b;
 });
+
 console.log(arrSort);
 const removed = arrSort.splice(DELETE_FROM_2_TO_4_ELEMENTS, THREE_ELEMENTS);
+
 console.log('видалені елементи');
 console.log(removed);
 console.log('результат після видаленя');
